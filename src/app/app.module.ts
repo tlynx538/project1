8f433769f5bcc2f1bbc060010f43be661f0eea14
Vinayak Jaiwant <vjaiwantx@gmail.com>
2021-11-07 13:57:54 +0530

    Added Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [NgbModule],
+    [NgbPaginationModule, NgbAlertModule],
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
